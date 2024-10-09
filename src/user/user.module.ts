05643bfe83999063d8c5f2690ac1639781f90d70
JAVOHIRQAYUMOV14050725 <qayumovjavohir2@gmail.com>
2024-10-09 10:45:20 +0500

    bilaolmadim

---
+++ src/user/user.module.ts
@@ -0,0 +1,18 @@
+// user/user.module.ts
+import { forwardRef, Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { UserService } from './user.service';
+import { UserController } from './user.controller';
+import { AuthModule } from '../auth/auth.module'; 
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    forwardRef(() => AuthModule)
+  ],
+  controllers: [UserController],
+  providers: [UserService],
+  exports: [UserService],
+})
+export class UserModule { }
